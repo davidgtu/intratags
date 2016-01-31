ed39606862532fbe232861fc13c54b31095c9801
spacebear5000 <david.giahuy.tu@gmail.com>
2016-01-30 22:18:04 -0500

    delete function is tough

--- app/assets/javascripts/angular/controllers/tags_controllers.js
+++ app/assets/javascripts/angular/controllers/tags_controllers.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('tagsapp', ['ngRoute', 'ngResource']);
 
 myApp.factory("Tag", function($resource) {
-  return $resource("/api/tags/:id", { id: "@_id"},
+  return $resource("/api/tags/:id", { _id: "@id"},
     {
       'create':  { method: 'POST' },
       'index':   { method: 'GET', isArray: true },
@@ -17,10 +17,6 @@ myApp.controller("TagListCtrl", ['$scope', '$resource', 'Tag', '$location',
                       function($scope, $resource, Tag, $location) {
   $scope.tags = Tag.query();
 
-  var changeSuccess = function() {
-    $location.path('/list');
-  };
-
   $scope.saveNewTag = function() {
     Tag.create({ tag: $scope.newTag }, function(response){
       $scope.tags.push(response);
@@ -29,29 +25,26 @@ myApp.controller("TagListCtrl", ['$scope', '$resource', 'Tag', '$location',
   }
 
   $scope.deleteTag = function(tagId) {
-    if(confirm("Are you sure you want to delete this tag?")){
-      debugger
-      Tag.delete({id: tagId}, function(){
-        var index = $scope.tags.indexOf(tagId);
-        $scope.tags.splice(index, 1)
-        $location.path('/')
-      });
-    }
+    Tag.remove({ _id: tagId }, function(response){
+      var index = $scope.tags.indexOf(tagId);
+      $scope.tags.splice(index, 1)
+      $location.path('/')
+    })
   };
 }]);
 
-myApp.controller("TagEditCtrl", ['$scope', '$resource', 'Tag', '$location', '$routeParams', function($scope, $resource, Tag, $location, $routeParams){
-  $scope.tag = Tag.get({ id: $routeParams.id })
-  $scope.update = function(){
-    if($scope.tagForm.$valid){
-      Tag.update({ id: $scope.tag.id }, { tag: $scope.tag }, function(){
-        $location.path('/');
-      }, function(error){
-        console.log(error)
-      });
-    }
-  }
-}]);
+// myApp.controller("TagEditCtrl", ['$scope', '$resource', 'Tag', '$location', '$routeParams', function($scope, $resource, Tag, $location, $routeParams){
+//   $scope.tag = Tag.get({ id: $routeParams.id });
+//   $scope.update = function(){
+//     if($scope.tagForm.$valid){
+//       Tag.update({ id: $scope.tag.id }, { tag: $scope.tag }, function(){
+//         $location.path('/');
+//       }, function(error){
+//         console.log(error)
+//       });
+//     }
+//   }
+// }]);
 
 // Routes
 // myApp.config([
